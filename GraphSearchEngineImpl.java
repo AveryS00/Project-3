cbcd854340798e143feffbe4d9cb9646d3a181d7
AveryS00 <44757867+AveryS00@users.noreply.github.com>
2018-11-12 23:41:19 -0500

    Add files via upload

--- GraphSearchEngineImpl.java
+++ GraphSearchEngineImpl.java
@@ -1,9 +1,40 @@
-import java.util.List;
+import java.util.*;
 
 public class GraphSearchEngineImpl implements GraphSearchEngine{
 	
-	//TODO
+	//TODO test and write up comments
 	public List<Node> findShortestPath(Node s, Node t) {
-		return null;
+		List<Node> visitedNodes = new ArrayList<Node>();
+		Queue<Node> nodesToVisit = new LinkedList<Node>();
+		Map<Node, Integer> distanceFromS = new HashMap<Node, Integer>();
+		List<Node> path = null;
+		
+		nodesToVisit.add(s);
+		distanceFromS.put(s, 0);
+		while (nodesToVisit.size() < 0) {
+			Node n = nodesToVisit.poll();
+			visitedNodes.add(n);
+			if (n.equals(s)) {
+				path = new ArrayList<Node>();
+				for (int i = 0; i < distanceFromS.get(n); i++) {
+					path.add(n);
+					for (Node j : n.getNeighbors()) {
+						if (visitedNodes.contains(j) && distanceFromS.get(j) == distanceFromS.get(n) - 1) {
+							n = j;
+							break;
+						}
+					}
+				}
+				break;
+			} else {
+				for (Node k : n.getNeighbors()) {
+					if (!visitedNodes.contains(k)) {
+						nodesToVisit.add(k);
+						distanceFromS.put(k, distanceFromS.get(n) + 1);
+					}
+				}
+			}
+		}
+		return path;
 	}
 }
